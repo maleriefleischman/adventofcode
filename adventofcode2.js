1b20a7d7692b4638ed5b560704fddc6bc5114623
Malerie Fleischman <mal.fleischman@gmail.com>
2018-05-15 20:37:32 -0700

    Update adventofcode2.js

--- adventofcode2.js
+++ adventofcode2.js
@@ -4,7 +4,7 @@ var instructions = ['URULLLLLRLDDUURRRULLLDURRDRDRDLURURURLDLLLLRUDDRRLUDDDDDDLR
 'DRUDRDURUURDLRLUUUUURUDLRDUURLLDUULDUULDLURDDUULDRDDRDULUDDDRRRRLDDUURLRDLLRLRURDRRRDURDULRLDRDURUDLLDDULRDUDULRRLLUDLLUUURDULRDDLURULRURDDLRLLULUDURDRRUDLULLRLDUDLURUDRUULDUDLRDUDRRDULDDLDRLRRULURULUURDULRRLDLDULULRUUUUULUURLURLRDLLRRRRLURRUDLRLDDDLDRDRURLULRDUDLRLURRDRRLRLLDLDDLLRRULRLRLRUDRUUULLDUULLDDRLUDDRURLRLDLULDURLLRRLDLLRDDDUDDUULLUDRUDURLLRDRUDLUDLLUDRUUDLRUURRRLLUULLUUURLLLRURUULLDLLDURUUUULDDDLRLURDRLRRRRRRUDLLLRUUULDRRDLRDLLDRDLDDLDLRDUDLDDRDDDDRULRRLRDULLDULULULRULLRRLLUURUUUDLDLUDUDDDLUUDDDDUDDDUURUUDRDURRLUULRRDUUDDUDRRRDLRDRLDLRRURUUDRRRUUDLDRLRDURD',
 'DDDLRURUDRRRURUUDLRLRDULDRDUULRURRRUULUDULDDLRRLLRLDDLURLRUDRLRRLRDLRLLDDLULDLRRURDDRDLLDDRUDRRRURRDUDULUDDULRRDRLDUULDLLLDRLUDRDURDRRDLLLLRRLRLLULRURUUDDRULDLLRULDRDLUDLULDDDLLUULRRLDDUURDLULUULULRDDDLDUDDLLLRRLLLDULRDDLRRUDDRDDLLLLDLDLULRRRDUDURRLUUDLLLLDUUULDULRDRULLRDRUDULRUUDULULDRDLDUDRRLRRDRLDUDLULLUDDLURLUUUDRDUDRULULDRDLRDRRLDDRRLUURDRULDLRRLLRRLDLRRLDLDRULDDRLURDULRRUDURRUURDUUURULUUUDLRRLDRDLULDURUDUDLUDDDULULRULDRRRLRURLRLRLUDDLUUDRRRLUUUDURLDRLRRDRRDURLLL'];
 
-
+//create a matrix array that can be traversed as new input is added
 var pad = [
 ['1', '2', '3'],
 ['4', '5', '6'],
@@ -17,37 +17,47 @@ for(j=0;j<pad.length;j++) {
 	}
 }
 
+// create a variable that can be added to as the code is uncovered
 var code = '';
 
-var l = 1;
-var m = 1;
+// vert = vertical directions (up vs down)
+var vert = 1;
+// horz = horizontal directions (left vs right)
+var horz = 1;
 
+/*traverse each letter within the matrix array.
+*
+*Since the keypad is only 3x3 the only possible values for the vert,horz variables are (0 , 1, 2)
+*
+*If vert= 0 the coressponding keynumber's are 1,2,3, if horz=0 the coressponding key numbers are 1,4,7 
+*/
 for(j=0;j<instructions.length;j++){
 	for(k=0;k<instructions[j].length;k++){
 			
 			if (instructions[j].charAt(k) == 'U') {
-				if (l != 0 ){
-					l--;
+				if (vert != 0 ){
+					vert--;
 					}
 				}
 			else if (instructions[j].charAt(k) == 'D' ) {
-				if (l != 2 ){
-					l++;
+				if (vert != 2 ){
+					vert++;
 					}
 				}
 			else if (instructions[j].charAt(k) == 'L' ) {
-				if ( m != 0 ){
-					m--;
+				if (horz != 0 ){
+					horz--;
 					}
 				}
 			else if ( instructions[j].charAt(k) == 'R' ) {
-				if ( m != 2 ){
-					m++;
+				if (horz != 2 ){
+					horz++;
 					}
 				}
 		
 		}
-		code = code + pad[l][m]
+	//each button to be pressed can be found by traversing the pad matrix with the vert,horz variables. Push value into the code array to compile entire keycode
+		code = code + pad[vert][horz]
 }
 
 alert(code)
